cf64a33a64eeda5f65eb778159704f9946f515a3
zxwang <zwang437@ucsc.edu>
2019-11-11 15:28:36 -0800

    fixed ListTest

--- PA3/ListTest.c
+++ PA3/ListTest.c
@@ -15,32 +15,33 @@ int main(int argc, char* argv[]){
    List A = newList();
    List B = newList();
    List C = newList();
-   int X[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
-   int i, u=-1, v=-2, equal=0;
+   long X[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+   int i, equal=0;
+   long u=-1, v=-2;
 
    // initialize Lists A and B
    for(i=1; i<=20; i++){
-      append(A, &X[i]);
-      prepend(B, &X[i]);
+      append(A, X[i]);
+      prepend(B, X[i]);
    }
 
    // print both lists of integers in forward direction
    for(moveFront(A); index(A)>=0; moveNext(A)){
-      printf("%d ", *(int*)get(A));
+      printf("%ld ", get(A));
    }
    printf("\n");
    for(moveFront(B); index(B)>=0; moveNext(B)){
-      printf("%d ", *(int*)get(B));
+      printf("%ld ", get(B));
    }
    printf("\n");
 
    // print both lists of integers in backward direction
    for(moveBack(A); index(A)>=0; movePrev(A)){
-      printf("%d ", *(int*)get(A));
+      printf("%ld ", get(A));
    }
    printf("\n");
    for(moveBack(B); index(B)>=0; movePrev(B)){
-      printf("%d ", *(int*)get(B));
+      printf("%ld ", get(B));
    }
    printf("\n");
 
@@ -52,7 +53,7 @@ int main(int argc, char* argv[]){
    }
    // print the copy in forward direction
    for(moveFront(C); index(C)>=0; moveNext(C)){
-      printf("%d ", *(int*)get(C));
+      printf("%ld ", get(C));
    }
    printf("\n");
 
@@ -69,19 +70,19 @@ int main(int argc, char* argv[]){
 
    moveFront(A);
    for(i=0; i<5; i++) moveNext(A); // at index 5
-   insertBefore(A, &u);            // now at index 6
+   insertBefore(A, u);            // now at index 6
    for(i=0; i<9; i++) moveNext(A); // at index 15
-   insertAfter(A, &v);             // doesn't change index
+   insertAfter(A, v);             // doesn't change index
    for(i=0; i<5; i++) movePrev(A); // at index 10
    delete(A);                      // index is now undefined
 
    // print A in forward and backward direction
    for(moveFront(A); index(A)>=0; moveNext(A)){
-      printf("%d ", *(int*)get(A));
+      printf("%ld ", get(A));
    }
    printf("\n");
    for(moveBack(A); index(A)>=0; movePrev(A)){
-      printf("%d ", *(int*)get(A));
+      printf("%ld ", get(A));
    }
    printf("\n");
 
