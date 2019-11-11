78856b31287e9f9a05d64c946da9cb810255456a
zxwang <zwang437@ucsc.edu>
2019-11-11 01:14:48 -0800

    fixed header file

--- PA3/BigInteger.h
+++ PA3/BigInteger.h
@@ -9,85 +9,44 @@
 #include <stdlib.h>
 
 // Exported type -------------------------------------------------------------
-// BigInteger reference type
-typedef struct BigIntegerObj* BigInteger;
+typedef struct BigIntegerObj* BigInteger; // BigInteger reference type
 
 // Constructors-Destructors ---------------------------------------------------
+BigInteger newBigInteger(); // Returns a reference to a new BigInteger object in the zero state.
 
-//newBigInteger()
-// Returns a reference to a new BigInteger object in the zero state.
-BigInteger newBigInteger();
-
-//freeBigInteger()
-// Frees heap memory associated with *pN, sets *pN to NULL.
-void freeBigInteger(BigInteger* pN);
-
+void freeBigInteger(BigInteger* pN); // Frees heap memory associated with *pN, sets *pN to NULL.
 
 // Access functions -----------------------------------------------------------
 
-// sign()
-// Returns -1 if N is negative, 1 if N is positive, and 0 if N is in the zero // state.
-int sign(BigInteger N);
+int sign(BigInteger N); // Returns -1 if N is negative, 1 if N is positive, and 0 if N is in the zero
+                        // state.
 
-// compare()
-// Returns -1 if A<B, 1 if A>B, and 0 if A=B.
-int compare(BigInteger A, BigInteger B);
+int compare(BigInteger A, BigInteger B); // Returns -1 if A<B, 1 if A>B, and 0 if A=B.
 
-// equals()
-// Return true (1) if A and B are equal, false (0) otherwise.
-int equals(BigInteger A, BigInteger B);
+int equals(BigInteger A, BigInteger B); // Return true (1) if A and B are equal, false (0) otherwise.
 
 // Manipulation procedures ----------------------------------------------------
 
-// makeZero()
-// Re-sets N to the zero state.
-void makeZero(BigInteger N);
-
-// negate()
-// Reverses the sign of N: positive <--> negative. Does nothing if N is in the
-// zero state.
-void negate(BigInteger N);
+void makeZero(BigInteger N); // Re-sets N to the zero state.
+void negate(BigInteger N);  // Reverses the sign of N: positive <--> negative. Does nothing if N is in the
+                            // zero state.
 
 // BigInteger Arithmetic operations -----------------------------------------------
 
-// stringToBigInteger()
-// Returns a reference to a new BigInteger object representing the decimal integer
-// represented in base 10 by the string s.
-// Pre: s is a non-empty string containing only base ten digits {0,1,2,3,4,5,6,7,8,9}
-// and an optional sign {+, -} prefix.
-BigInteger stringToBigInteger(char* s);
+BigInteger stringToBigInteger(char* s); // Returns a reference to a new BigInteger object representing the decimal integer
+                                        // represented in base 10 by the string s.
+                                        // Pre: s is a non-empty string containing only base ten digits {0,1,2,3,4,5,6,7,8,9}
+                                        // and an optional sign {+, -} prefix.
 
-// copy()
-// Returns a reference to a new BigInteger object in the same state as N.
-BigInteger copy(BigInteger N);
+BigInteger copy(BigInteger N); // Returns a reference to a new BigInteger object in the same state as N.
 
+BigInteger sum(BigInteger A, BigInteger localB); // Returns a reference to a new BigInteger object representing A + B.
 
-// sum()
-// Returns a reference to a new BigInteger object representing A + B.
-BigInteger sum(BigInteger A, BigInteger localB);
+BigInteger diff(BigInteger A, BigInteger B); // Returns a reference to a new BigInteger object representing A - B.
 
-
-
-// diff()
-// Returns a reference to a new BigInteger object representing A - B.
-BigInteger diff(BigInteger A, BigInteger B);
-
-
-// prod()
-// Returns a reference to a new BigInteger object representing A*B
-BigInteger prod(BigInteger A, BigInteger B);
+BigInteger prod(BigInteger A, BigInteger B); // Returns a reference to a new BigInteger object representing A*B.
 
 // Other operations -----------------------------------------------------------
-// printBigInteger()
-// Prints a base 10 string representation of N to filestream out.
-void printBigInteger(FILE* out, BigInteger N);
-
-
-
-
-
-
-
-
+void printBigInteger(FILE* out, BigInteger N);  // Prints a base 10 string representation of N to filestream out.
 
 #endif /* BigInteger_h */
